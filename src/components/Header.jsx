0a9fc1ff17b7ec39b186f213997680c016797bb0
Maxwell Williams <awoonor1z@gmail.com>
2025-04-01 08:56:31 +0000

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header> 
+            <img src="src\assets\chef-claude-icon.png" alt="chef claude logo" />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
